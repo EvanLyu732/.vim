209748632df1ea12f410d5f4ec1f670b44533731
EvanLyu732 <zhengshushu1997@gmail.com>
2023-01-03 22:43:37 +0800

    add setup sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+pushd $HOME > /dev/null; 
+   if [[ ! -d .vim  ]]; then
+		   git clone https://github.com/EvanLyu732/.vim.git 
+   else 
+		   echo ".vim folder exist."
+
+   fi 
+popd > /dev/null;
